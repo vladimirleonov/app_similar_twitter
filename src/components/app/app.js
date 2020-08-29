b16845721047c42859c74392846f905e81a21969
vladimir <vladimirleonov789@gmail.com>
2020-08-29 11:08:56 +0300

    make post-status-filter component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import AppHeader from '../app-header/';
 import SearchPanel from '../search-panel/';
+import PostStatusFilter from '../post-status-filter/';
 
 const App = () => {
    return (
       <div className="app">
          <AppHeader />
          <div className="search-panel d-flex">
-            <SearchPanel/>
+            <SearchPanel />
+            <PostStatusFilter/>
          </div>
       </div>
    )
---
+++ src/components/post-status-filter/post-status-filter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PostStatusFilter = () => {
+   return (
+      <div className="btn-group">
+         <button>Все</button>
+         <button>Понравились</button>
+      </div>
+   )
+}
+
+export default PostStatusFilter;
