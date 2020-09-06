7acba4db381ab981592376a7164b52ee3855d41b
vladimir <vladimirleonov789@gmail.com>
2020-09-06 20:22:10 +0300

    add classes buttons

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -4,8 +4,8 @@ import './post-status-filter.css';
 const PostStatusFilter = () => {
    return (
       <div className="btn-group">
-         <button>Все</button>
-         <button>Понравились</button>
+         <button type="button" className="btn btn-info">Все</button>
+         <button type="button" className="btn btn-outline-secondary">Понравились</button>
       </div>
    )
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/';
 
+// class WhyAmI extends Component {
+//    constructor(props) {
+//       super(props);
+//       this.state = {
+//          years: 26
+//       }
+//       this.nextYears = this.nextYears.bind(this);
+//    }
+//    nextYears() {
+//       this.setState(state => ({
+//          years: ++state.years
+//       }))                                                                
+//    }
+//    render () {
+//       const { name, surname, link } = this.props;
+//       const { years } = this.state;
+//       return (
+//          <>
+//             <h1>my name is {name}, surname - {surname}, years = {years}</h1>
+//             <a href={link}>my profile</a>
+//             <button onClick = {this.nextYears}>++</button>
+//          </>
+//       )
+//    }   
+// }
+
+// const App = () => {
+//    return (
+//       <>
+//          <WhyAmI name="Ivan" surname="Valyev" link="https://www.youtube.com/" />
+//          <WhyAmI name="Evgeniy" surname="Malcey" link="https://www.youtube.com/" />
+//          <WhyAmI name = "Nikolay" surname = "Olha" link = "https://www.youtube.com/"/>
+//       </>
+//    )
+// }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
