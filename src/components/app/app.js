6ecd7acaa26c0995f445f557e32d0f5cb76f8286
vladimir <vladimirleonov789@gmail.com>
2020-09-06 21:16:55 +0300

    imitated data from the server

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,6 +8,12 @@ import PostAddForm from '../post-add-form/';
 import './app.css';
 
 const App = () => {
+   const data = [
+      {label: 'Going to learn React', important: true},
+      {label: 'That is so good', important: true},
+      { label: 'I need a break...', important: false },
+      {label: 'I need this kurs', important: false}
+   ]
    return (
       <div className="app">
          <AppHeader />
@@ -15,7 +21,7 @@ const App = () => {
             <SearchPanel />
             <PostStatusFilter/>
          </div>
-         <PostList />
+         <PostList posts={data}/>
          <PostAddForm/>
       </div>
    )
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,13 +2,29 @@ import React from 'react';
 import PostListItem from '../post-list-item/';
 import './post-list.css';
 
-const PostList = () => {
+const PostList = ({posts}) => {
+
+   const elements = posts.map((item) => {
+      return (
+         <li className="list-group-item">
+            <PostListItem
+               label={item.label}
+               important={item.important} />
+         </li>
+      )
+   })
+
    return (
-      <ul className="app-list list-group"> 
-         <PostListItem label = "Going to learn React"/>
-         <PostListItem label="That is so good" important/>
-         <PostListItem label = "I need a break..."/>
+      // <ul className="app-list list-group"> 
+      //    <PostListItem label = "Going to learn React"/>
+      //    <PostListItem label="That is so good" important={true}/>
+      //    <PostListItem label="I need a break..." />
+      //    <PostListItem label = "I need this kurs"/>
+      // </ul>
+      <ul className="app-list list-group">
+         {elements}
       </ul>
+
    )
 }
 
